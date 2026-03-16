54db9fac481fa7535325449ff3e09919bbc19b9c
EduardoSanz <eduardo11sannz@gmail.com>
2026-03-16 16:40:41 -0600

    feat: implementar variables de entorno y seguridad

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -43,10 +43,9 @@ const ContactForm = () => {
     
     try {
       // Placeholder credentials - User to fill later
-      const SERVICE_ID = 'service_6imrcie';
-      const TEMPLATE_ID = 'template_nm8dlfg';
-      const PUBLIC_KEY = 'U7tKwPyLJYtvV_RWP';
-
+      const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+      const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+      const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       await emailjs.sendForm(
         SERVICE_ID,
         TEMPLATE_ID,
